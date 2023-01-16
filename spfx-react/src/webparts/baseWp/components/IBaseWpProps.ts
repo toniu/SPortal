c98a9e875fd556808c4f9f37dfae9c9aa56c06b7
toniu <zhac032@live.rhul.ac.uk>
2023-01-16 19:29:55 +0000

    Alternative frontend set-up plan: SPFX React project; initial set-up of webpart.

---
+++ spfx-react/src/webparts/baseWp/components/IBaseWpProps.ts
@@ -0,0 +1,7 @@
+export interface IBaseWpProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ spfx-react/src/webparts/baseWp/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IBaseWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'BaseWpWebPartStrings' {
+  const strings: IBaseWpWebPartStrings;
+  export = strings;
+}
