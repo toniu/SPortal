27c4318e2ed68f4c1c46e8fb98be06a6980662d9
toniu <zhac032@live.rhul.ac.uk>
2023-02-20 00:45:58 +0000

    Initial UI design on the feed webpart - placeholder data included.

---
+++ spfx-react/src/webparts/feed/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFeedWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FeedWebPartStrings' {
+  const strings: IFeedWebPartStrings;
+  export = strings;
+}
