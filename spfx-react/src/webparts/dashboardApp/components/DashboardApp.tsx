e591373c785de89c3581ae489110d7190f477aaf
toniu <zhac032@live.rhul.ac.uk>
2023-02-24 01:23:33 +0000

    Functionality on read operation of Sharepoint lists; retrieveing the upcoming and recent (max. 3)  events to render as overview on dashboard.

--- spfx-react/src/webparts/dashboardApp/components/DashboardApp.tsx
+++ spfx-react/src/webparts/dashboardApp/components/DashboardApp.tsx
@@ -11,6 +11,8 @@ import { faFile } from "@fortawesome/free-solid-svg-icons"
 
 export default class DashboardApp extends React.Component<IDashboardAppProps, {}> {
   private _sp:SPFI;
+  private recentEvents: any = null;
+  private upcomingEvents: any = null;
 
   constructor(props: IDashboardAppProps) {
     super(props);
@@ -18,23 +20,68 @@ export default class DashboardApp extends React.Component<IDashboardAppProps, {}
     this._sp = getSP(props.context);
   }
 
-  /* Get all items */
+  /* Get events from SP Lists */
   private getAllEvents = async () => {
     try {
       /* Recreate list, columns are messed up at the moment */
-      /* Get list of all events */
-      const events: any[] = await this._sp.web.lists.getByTitle("Events").items();
-
+      /* Get list of all events; ordered by date */
+      const events: any[] = await this._sp.web.lists.getByTitle("Events").items.select().orderBy("field_3", true)();
       console.log("SUCCESS: ", events);
-      
-      /* Get list of all events */
+
+      const today = new Date().getTime();
+
       /* Filtered list of 3 events max. past today's date */
+      let eventsBefore = events.filter(event => Date.parse(event.field_3) < today);
+      eventsBefore = eventsBefore.slice(0, 3)
+      console.log('EA', eventsBefore)
+
       /* Filtered list of 3 events max. after today's date */
-      /* Use filtered lists to present recent meetings and upcoming meetings */
+      let eventsAfter = events.filter(event => Date.parse(event.field_3) >= today);
+      eventsAfter = eventsAfter.slice(Math.max(eventsAfter.length - 3, 0))
+      console.log('EA', eventsAfter)
+      /* Format the dates of the recent and upcoming events */
+      for (let i = 0; i < eventsBefore.length; i++) {
+        eventsBefore[i].field_3 = new Date(Date.parse(eventsBefore[i].field_3))
+      }
+      for (let i = 0; i < eventsAfter.length; i++) {
+        eventsAfter[i].field_3 = new Date(Date.parse(eventsAfter[i].field_3))
+      }
+       /* Use filtered lists to render recent meetings and upcoming meetings */
+      this.recentEvents = this.renderRecentEvents(eventsBefore);
+      this.upcomingEvents = this.renderUpcomingEvents(eventsAfter);
+      /* Re-render based on recent and upcoming events gathered */
+      this.forceUpdate()
 
     } catch (e) {
       console.error("ERROR: ", e);
     }
+  }
+
+  public getRecentPoll() {
+    return;
+  }
+
+  public renderRecentEvents(events: any) {
+    const recentEvents = events.map((e: any) => <li key={e.ID} className="p-2 m-1 block border-l-4 border-black bg-white">
+    Hosted by <span className="font-bold"> {e.field_4},</span> <span className="block font-normal">
+    {e.field_3.toLocaleDateString()}
+      <span className="font-semibold"> {e.field_3.toLocaleTimeString()} </span>
+    </span>
+  </li>)
+    return recentEvents;
+  }
+
+  public renderUpcomingEvents(events: any) {
+    const upcomingEvents = events.map((e: any) => <div key={e.ID} className="m-1 w-1/3 text-sm">
+    <h3 className="p-1 uppercase text-white text-center font-semibold bg-gray-900">
+    {e.field_3.toLocaleDateString()}
+    </h3>
+    <div className="p-2 block text-center bg-white">
+      <span className="p-2 my-1"> {e.field_1},</span>
+      <span className="block p-2 font-semibold"> {e.field_3.toLocaleTimeString()} </span>
+    </div>
+  </div>)
+    return upcomingEvents;
   }
 
   public async componentDidMount(): Promise<void> {
@@ -50,24 +97,7 @@ export default class DashboardApp extends React.Component<IDashboardAppProps, {}
             recent meetings
           </h2>
           <ul className="p-2">
-            <li className="p-2 m-1 block border-l-4 border-black bg-white">
-              Hosted by <span className="font-bold"> J.Turner,</span> <span className="block font-normal">
-                16/12/22
-                <span className="font-semibold"> 17:00 </span>
-              </span>
-            </li>
-            <li className="p-2 m-1 block border-l-4 border-black bg-white">
-              Hosted by <span className="font-bold"> D.Micheals,</span> <span className="block font-normal">
-                18/12/22
-                <span className="font-semibold"> 17:00 </span>
-              </span>
-            </li>
-            <li className="p-2 m-1 block border-l-4 border-black bg-white">
-              Hosted by <span className="font-bold"> T.Andrews,</span> <span className="block font-normal">
-                23/12/22
-                <span className="font-semibold"> 17:00 </span>
-              </span>
-            </li>
+            {this.recentEvents}
           </ul>
         </div>
         <div className="p-1 m-1 w-3/5 bg-gray-300 border-t-4 border-indigo-600">
@@ -75,33 +105,7 @@ export default class DashboardApp extends React.Component<IDashboardAppProps, {}
             upcoming events
           </h2>
           <div className="p-1 flex">
-            <div className="m-1 w-1/3 text-sm">
-              <h3 className="p-1 uppercase text-white text-center font-semibold bg-gray-900">
-                Mon 21 Nov
-              </h3>
-              <div className="p-2 block text-center bg-white">
-                <span className="p-2 my-1">Catch-up meeting,</span>
-                <span className="block p-2 font-semibold"> 10:30 </span>
-              </div>
-            </div>
-            <div className="m-1 w-1/3 text-sm">
-              <h3 className="p-1 uppercase text-white text-center font-semibold bg-gray-900">
-                Tue 22 Nov
-              </h3>
-              <div className="p-2 block text-center bg-white">
-                <span className="p-2 my-1">Audit meeting,</span>
-                <span className="block p-2 font-semibold"> 10:30 </span>
-              </div>
-            </div>
-            <div className="m-1 w-1/3 text-sm">
-              <h3 className="p-1 uppercase text-white text-center font-semibold bg-gray-900">
-                Fri 25 Nov
-              </h3>
-              <div className="p-2 block text-center bg-white">
-                <span className="p-2 my-1">End of week meeting,</span>
-                <span className="block p-2 font-semibold"> 10:30 </span>
-              </div>
-            </div>
+            {this.upcomingEvents}
           </div>
           <br/>
           <button type="button" className="px-3 py-1 mx-4 block float-right text-normal bg-indigo-900 rounded-full text-white hover:bg-indigo-600 transition 0.2s">
