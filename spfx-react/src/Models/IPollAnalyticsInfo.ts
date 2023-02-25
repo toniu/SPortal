c3569b1ff03b1f905071bbfdcc2a765cc12c44c8
toniu <zhac032@live.rhul.ac.uk>
2023-02-25 16:10:30 +0000

    New webpart from github samples (for help): attempt to integrate group member management with Sharepoint.

--- spfx-react/src/Models/IPollAnalyticsInfo.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ChartType } from "@pnp/spfx-controls-react/lib/ChartControl";
-
-export interface IPollAnalyticsInfo {
-  Question: string;
-  Labels: string[];
-  PollResponse: any[];
-  ChartType: ChartType;
-}
--- spfx-react/src/webparts/groups/components/IGroupsProps.ts
+++
@@ -1,7 +0,0 @@
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-
-export interface IGroupsProps {
-  description: string;
-  userDisplayName: string;
-  spcontext: WebPartContext;
-}
